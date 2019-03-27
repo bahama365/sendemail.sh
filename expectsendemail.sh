7f255e41b962cee7e497d2ac7bd2afd4548592ff
bahama365 <31001392+bahama365@users.noreply.github.com>
2019-03-27 11:33:22 +0000

    Update expectsendemail.sh

--- expectsendemail.sh
+++ expectsendemail.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/expect -f
 #
+# If you have special chars in your password you will need to escape them with a \ (i.e \$)
+# As yourpassword is stored in clear here, ensure you set correct file permissions
 set timeout -1
 spawn ./sendemailexample.sh
 expect "Password:"
