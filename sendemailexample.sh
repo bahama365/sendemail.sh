445bab739007cd6f1e33565770c33d0715833a07
bahama365 <31001392+bahama365@users.noreply.github.com>
2019-03-27 11:31:57 +0000

    Update sendemailexample.sh

--- sendemailexample.sh
+++ sendemailexample.sh
@@ -3,5 +3,11 @@
 # To stop getting the SSL error when sending to Gmail,
 # Edit SSL_verify_mode => SSL_VERIFY_PEER in /usr/share/perl5/vendor_perl/IO/Socket/SSL.pm
 
-sendemail -f "gstwhetbs@gmail.com" -u "test email 4" -t "chanscombe@icloud.com" -s "smtp.gmail.com:587" -o tls=yes -xu gstwhetbs@gmail.com -o message-file=data.txt -a data.txt
+sendemail -f "address@gmail.com" -u "test email 4" -t "recipient@icloud.com" -s "smtp.gmail.com:587" \
+-o tls=yes -xu address@gmail.com -o message-file=data.txt -a data.txt
 
+-t is 'to'
+-u is subject
+-f and -xu should be the same (From and Username)
+-o reads the mail body from a file
+-a adds an attachement
